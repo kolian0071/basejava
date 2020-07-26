242d3ca18a397ab8a916613920dae62de017b529
Nikolay Frolkov <kolian0071@gmail.com>
2020-07-26 21:32:20 +0300

    реализовал методы класса ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,50 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        for (Resume value : storage) {
+            if (value != null) {
+                if (uuid.equals(value.uuid)) {
+                    resume = value;
+                    break;
+                }
+            }
+        }
+        return resume;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                if (uuid.equals(storage[i].uuid)) {
+                    storage[i] = null;
+                    break;
+                }
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, storage.length);
     }
 
     int size() {
-        return 0;
+        return storage.length;
     }
 }
