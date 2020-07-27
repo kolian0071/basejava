d4ce48eacbd8dd6aec713c32ddb1b8019d268bd9
Nikolay Frolkov <kolian0071@gmail.com>
2020-07-27 23:59:39 +0300

    упростил метод delete(), добавил переменную size

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,28 +5,23 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = size();
 
     void clear() {
-        Arrays.fill(storage, 0, size(), null);
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                break;
-            }
-        }
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
         Resume resume = null;
-        for (Resume value : storage) {
-            if (value != null) {
-                if (uuid.equals(value.uuid)) {
-                    resume = value;
-                    break;
-                }
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i])) {
+                resume = storage[i];
             }
         }
         return resume;
@@ -34,40 +29,20 @@ public class ArrayStorage {
 
     void delete(String uuid) {
         int index = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                if (uuid.equals(storage[i].uuid)) {
-                    index = i;
-                    break;
-                }
+        boolean delete = false;
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                index = i;
+                delete = true;
             }
         }
-        if (index == 0) {
-            Resume[] storageCopy = new Resume[storage.length-1];
-            System.arraycopy(storage, 1, storageCopy, 0, storageCopy.length);
-            storage = storageCopy;
-        }
-        else if (index == storage.length-1) {
-            Resume[] storageCopy = new Resume[storage.length-1];
-            for (int i = 0; i < storage.length-1; i++) {
-                storageCopy[i] = storage[i];
+        if (delete) {
+            for (int i = index; i < size - 1; i++) {
+                storage[i] = storage[i + 1];
             }
-            storage = storageCopy;
-        }
-        else {
-            Resume[] storageCopy = new Resume[storage.length-1];
-            for (int i = 0; i < storage.length; i++) {
-                if (i > index) {
-                    storageCopy[i-1] = storage[i];
-                }
-                if (i == index) {
-                    continue;
-                }
-                if (i < index) {
-                    storageCopy[i] = storage[i];
-                }
-            }
-            storage = storageCopy;
+            storage[size - 1] = null;
+            delete = false;
+            size--;
         }
     }
 
@@ -75,7 +50,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, size());
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
