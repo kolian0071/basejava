db56d24470cab34329a7a5768d8e1f091676803d
Nikolay Frolkov <kolian0071@gmail.com>
2020-07-28 22:54:23 +0300

    изменил реализацию метода size

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    private int size = size();
+    private int size = 0;
 
     void clear() {
         Arrays.fill(storage, 0, size, null);
@@ -28,20 +28,17 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        int index = 0;
-        boolean delete = false;
+        int index = -1;
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].uuid)) {
                 index = i;
-                delete = true;
             }
         }
-        if (delete) {
+        if (index >= 0) {
             for (int i = index; i < size - 1; i++) {
                 storage[i] = storage[i + 1];
             }
             storage[size - 1] = null;
-            delete = false;
             size--;
         }
     }
@@ -54,12 +51,6 @@ public class ArrayStorage {
     }
 
     int size() {
-        int size = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                size++;
-            }
-        }
         return size;
     }
 }
